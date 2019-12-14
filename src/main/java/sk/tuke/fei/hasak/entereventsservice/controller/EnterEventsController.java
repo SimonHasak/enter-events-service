2d5c02d842753e1690b0118c51c8817d8272a932
xhasak <456221@mail.muni.cz>
2019-12-14 12:44:36 +0100

    Updated url addresses to match REST endpoints, added author and license.

--- src/main/java/sk/tuke/fei/hasak/entereventsservice/controller/EnterEventsController.java
+++ src/main/java/sk/tuke/fei/hasak/entereventsservice/controller/EnterEventsController.java
@@ -5,8 +5,46 @@
 
 package sk.tuke.fei.hasak.entereventsservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import sk.tuke.fei.hasak.entereventsservice.model.Event;
+import sk.tuke.fei.hasak.entereventsservice.service.EnterEventsService;
+
 /**
  * @author Šimon Hašák
  */
+@RestController
+@RequestMapping("/enter-events")
+@Slf4j
+@RequiredArgsConstructor
 public class EnterEventsController {
+
+
+    private final EnterEventsService eventsService;
+
+
+    @GetMapping()
+    public Iterable<Event> findAll() {
+        return eventsService.findAll();
+    }
+
+    @SneakyThrows
+    @GetMapping("/{id}")
+    public Event findById(@PathVariable Long id) {
+        return eventsService.findById(id);
+    }
+
+    @PostMapping()
+    public Event save(@RequestBody Event event) { return eventsService.save(event); }
+
+    @SneakyThrows
+    @PutMapping("/{id}")
+    public void update(@RequestBody Event event, @PathVariable Long id) { eventsService.update(event, id); }
+
+    @SneakyThrows
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { eventsService.deleteById(id); }
+
 }
