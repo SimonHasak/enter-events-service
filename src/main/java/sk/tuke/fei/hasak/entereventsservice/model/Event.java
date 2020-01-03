9e142d473c59c41f989de6ea2d44dfdd62829246
xhasak <456221@mail.muni.cz>
2020-01-03 23:26:53 +0100

    Added documentation.

--- src/main/java/sk/tuke/fei/hasak/entereventsservice/model/Event.java
+++ src/main/java/sk/tuke/fei/hasak/entereventsservice/model/Event.java
@@ -6,17 +6,20 @@
 package sk.tuke.fei.hasak.entereventsservice.model;
 
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 /**
+ * The type Event.
+ *
  * @author Šimon Hašák
  */
 @Entity
 @Data
 @Table(name = "events")
-public class Event {
+public class Event extends RepresentationModel<Event> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
