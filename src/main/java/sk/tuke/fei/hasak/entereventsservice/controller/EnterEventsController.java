7b125a73489a1da38e1252a357f24f993ac6a203
xhasak <456221@mail.muni.cz>
2020-01-03 23:25:53 +0100

    HATEOAS extension implemented.

--- src/main/java/sk/tuke/fei/hasak/entereventsservice/controller/EnterEventsController.java
+++ src/main/java/sk/tuke/fei/hasak/entereventsservice/controller/EnterEventsController.java
@@ -7,17 +7,27 @@ package sk.tuke.fei.hasak.entereventsservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 import sk.tuke.fei.hasak.entereventsservice.model.Event;
 import sk.tuke.fei.hasak.entereventsservice.service.EnterEventsService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+
 /**
+ * The type Enter events controller.
+ *
  * @author Šimon Hašák
  */
 @RestController
 @RequestMapping("/enter-events")
-@Slf4j
 @RequiredArgsConstructor
 public class EnterEventsController {
 
@@ -25,24 +35,78 @@ public class EnterEventsController {
     private final EnterEventsService eventsService;
 
 
+    /**
+     * Find all iterable events.
+     *
+     * @return the iterable events.
+     */
     @GetMapping()
-    public Iterable<Event> findAll() {
-        return eventsService.findAll();
+    public CollectionModel<EntityModel<Event>> findAll() {
+        List<EntityModel<Event>> events = StreamSupport.stream(eventsService.findAll().spliterator(), false)
+                .map(event -> new EntityModel<>(event,
+                        linkTo(methodOn(EnterEventsController.class).findById(event.getId())).withSelfRel(),
+                        linkTo(EnterEventsController.class).withSelfRel()))
+                .collect(Collectors.toList());
+
+        return new CollectionModel<>(events);
     }
 
+    /**
+     * Find event by id.
+     *
+     * @param id the id of event
+     * @return the event.
+     */
     @SneakyThrows
     @GetMapping("/{id}")
-    public Event findById(@PathVariable Long id) {
-        return eventsService.findById(id);
+    public EntityModel<Event> findById(@PathVariable Long id) {
+
+        Event event = eventsService.findById(id);
+
+        event.add(linkTo(methodOn(EnterEventsController.class).findById(event.getId())).withSelfRel(),
+                linkTo(EnterEventsController.class).withSelfRel());
+
+        return new EntityModel<>(event);
     }
 
+    /**
+     * Save event.
+     *
+     * @param event the event to be save.
+     * @return the event
+     */
     @PostMapping()
-    public Event save(@RequestBody Event event) { return eventsService.save(event); }
+    public EntityModel<Event> save(@RequestBody Event event) {
+        Event savedEvent = eventsService.save(event);
 
+        savedEvent.add(linkTo(methodOn(EnterEventsController.class).findById(event.getId())).withSelfRel(),
+                linkTo(EnterEventsController.class).withSelfRel());
+
+        return new EntityModel<>(savedEvent);
+    }
+
+    /**
+     * Update event.
+     *
+     * @param event the new event
+     * @param id    the id of old event to be updated by new one.
+     */
     @SneakyThrows
     @PutMapping("/{id}")
-    public void update(@RequestBody Event event, @PathVariable Long id) { eventsService.update(event, id); }
+    public EntityModel<Event> update(@RequestBody Event event, @PathVariable Long id) {
+        Event updatedEvent = eventsService.update(event, id);
 
+        updatedEvent.add(linkTo(methodOn(EnterEventsController.class).findById(updatedEvent.getId())).withSelfRel(),
+                linkTo(EnterEventsController.class).withSelfRel());
+
+        return new EntityModel<>(updatedEvent);
+    }
+
+    /**
+     * Delete event by its id.
+     *
+     * @param id the id.
+     */
     @SneakyThrows
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) { eventsService.deleteById(id); }
