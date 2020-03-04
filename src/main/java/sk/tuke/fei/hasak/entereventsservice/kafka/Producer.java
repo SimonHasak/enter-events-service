af7119191d26e31731d67d02d516f085b275af3f
Simon Hasak <simon.hasak@gmail.com>
2020-03-04 17:24:29 +0100

    Implemented kafka producer with kafkaTemplate

--- src/main/java/sk/tuke/fei/hasak/entereventsservice/kafka/Producer.java
+++ src/main/java/sk/tuke/fei/hasak/entereventsservice/kafka/Producer.java
@@ -1,4 +1,28 @@
 package sk.tuke.fei.hasak.entereventsservice.kafka;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class Producer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${saved.event.topic}")
+    private String topic;
+
+    @Autowired
+    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendSavedEventMssg(String message) {
+        this.kafkaTemplate.send(topic, message);
+        log.info("[Enter events service] message was sent.");
+    }
+
 }
