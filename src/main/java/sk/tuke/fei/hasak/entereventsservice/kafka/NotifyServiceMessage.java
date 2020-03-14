91f796ceec5b33744a82b53d165914c1330b2aee
Simon Hasak <simon.hasak@gmail.com>
2020-03-14 11:38:05 +0100

    Message class for exchanging data in json format

--- src/main/java/sk/tuke/fei/hasak/entereventsservice/kafka/NotifyServiceMessage.java
+++ src/main/java/sk/tuke/fei/hasak/entereventsservice/kafka/NotifyServiceMessage.java
@@ -1,4 +1,19 @@
 package sk.tuke.fei.hasak.entereventsservice.kafka;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotifyServiceMessage {
+
+
+    private String message;
+
+    private String email;
+
 }
--- src/main/java/sk/tuke/fei/hasak/entereventsservice/kafka/SavedEventMessage.java
+++ src/main/java/sk/tuke/fei/hasak/entereventsservice/kafka/SavedEventMessage.java
@@ -2,26 +2,22 @@ package sk.tuke.fei.hasak.entereventsservice.kafka;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Getter
 @ToString
-public class Foo {
+@NoArgsConstructor
+@AllArgsConstructor
+public class SavedEventMessage {
 
-    @Id
     private long id;
 
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime time = LocalDateTime.now();
 
-    public Foo() { }
-
-    public Foo(long id, LocalDateTime time) {
-        this.id = id;
-        this.time = time;
-    }
 }
