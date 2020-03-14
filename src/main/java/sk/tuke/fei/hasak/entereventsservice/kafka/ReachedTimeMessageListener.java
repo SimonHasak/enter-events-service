bff4f53e0ac5229b203d16a380b903a3556aad1e
Simon Hasak <simon.hasak@gmail.com>
2020-03-14 11:39:42 +0100

    Consumer added for ReachedTimeMessage

--- src/main/java/sk/tuke/fei/hasak/entereventsservice/kafka/ReachedTimeMessageListener.java
+++ src/main/java/sk/tuke/fei/hasak/entereventsservice/kafka/ReachedTimeMessageListener.java
@@ -1,27 +1,44 @@
 package sk.tuke.fei.hasak.entereventsservice.kafka;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import sk.tuke.fei.hasak.entereventsservice.model.Event;
+import sk.tuke.fei.hasak.entereventsservice.service.EnterEventsService;
 
 @Slf4j
 @Component
-public class Consumer {
+public class ReachedTimeMessageListener {
 
-//    @Autowired
-//    private KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, NotifyServiceMessage> kafkaTemplate;
 
-//    @Value("${send.mssg.to.notify.service}")
-//    private String topic;
+    private EnterEventsService eventsService;
 
-//    @KafkaListener(topics = "${saved.event.topic}")
-//    public void processMessage(String message) {
-//        log.info("[Enter events service] message was received");
-////        kafkaTemplate.send(topic, "peso");
-////        log.info("[Enter events service] message was sent to notify-service");
-//    }
+    @Value("${kafka.notify.service.event}")
+    private String topic;
+
+    @Autowired
+    public ReachedTimeMessageListener(KafkaTemplate<String, NotifyServiceMessage> kafkaTemplate, EnterEventsService eventsService) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.eventsService = eventsService;
+    }
+
+    @SneakyThrows
+    @KafkaListener(topics = "${kafka.reached.time.event}", groupId = "${kafka.group.reached.time.receiver}")
+    public void processMessage(ReachedTimeMessage message) {
+        log.info("[Enter events service] received:{}", message);
+
+        Event event = eventsService.findById(message.getId());
+
+        NotifyServiceMessage notifyMessage = new NotifyServiceMessage(event.getMessage(), event.getEmail());
+
+        kafkaTemplate.send(topic, notifyMessage);
+
+        log.info("[Enter events service] send:{}", notifyMessage);
+    }
 
 }
