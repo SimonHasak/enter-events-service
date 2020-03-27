2c14788cb321a30570a7f147cb11955fd13b0af8
Simon Hasak <simon.hasak@gmail.com>
2020-03-27 11:00:45 +0100

    New configuration for kafka producer

---
+++ src/main/java/sk/tuke/fei/hasak/entereventsservice/config/KafkaProducerConfig.java
@@ -0,0 +1,37 @@
+package sk.tuke.fei.hasak.entereventsservice.config;
+
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import sk.tuke.fei.hasak.entereventsservice.kafka.SavedEventMessage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value(value = "${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ProducerFactory<String, SavedEventMessage> savedEventMessageProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, SavedEventMessage> savedEventMessageKafkaTemplate() {
+        return new KafkaTemplate<>(savedEventMessageProducerFactory());
+    }
+
+}
