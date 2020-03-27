a7197529ae571ff51ebc156f3d9ce000e21eac95
Simon Hasak <simon.hasak@gmail.com>
2020-03-27 17:15:02 +0100

    Deleted unusued imports

--- src/main/java/sk/tuke/fei/hasak/entereventsservice/kafka/SavedEventMessageProducer.java
+++ src/main/java/sk/tuke/fei/hasak/entereventsservice/kafka/SavedEventMessageProducer.java
@@ -4,9 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
